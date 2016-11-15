71ca0fecb28646527bb5d0a8d777d6eb73744950
Nathan Smith <nathansforyou@gmail.com>
2016-11-14 21:52:03 -0500

    Added new PCA function, need to compared against existing

--- protein_pyspark/protein_demo.py
+++ protein_pyspark/protein_demo.py
@@ -33,3 +33,91 @@ df = sqlContext.createDataFrame(data, ["features"])
 pca = PCA(k=2, inputCol="features", outputCol="pca_features")
 model = pca.fit(df)
 model.transform(df).collect()[0].pca_features
+
+
+data = [(Vectors.dense([1.0, 0.0]),), (Vectors.dense([0.0, -1.0]),)]
+
+
+### NEW PCA MODEL TO GET COMPONENTS AND EIGENVALUES
+import numpy as np
+
+def estimateCovariance(df):
+    """Compute the covariance matrix for a given dataframe.
+
+    Note:
+        The multi-dimensional covariance array should be calculated using outer products.  Don't
+        forget to normalize the data by first subtracting the mean.
+
+    Args:
+        df:  A Spark dataframe with a column named 'features', which (column) consists of DenseVectors.
+
+    Returns:
+        np.ndarray: A multi-dimensional array where the number of rows and columns both equal the
+            length of the arrays in the input dataframe.
+    """
+    m = df.select(df['features']).map(lambda x: x[0]).mean()
+    dfZeroMean = df.select(df['features']).map(lambda x:   x[0]).map(lambda x: x-m)  # subtract the mean
+    return dfZeroMean.map(lambda x: np.outer(x,x)).sum()/df.count()
+
+
+from numpy.linalg import eigh
+
+def pca(df, k=2):
+    """Computes the top `k` principal components, corresponding scores, and all eigenvalues.
+
+    Note: from pyspark.ml.feature import *
+        All eigenvalues should be returned in sorted order (largest to smallest). `eigh` returns
+        each eigenvectors as a column.  This function should also return eigenvectors as columns.
+
+    Args:
+        df: A Spark dataframe with a 'features' column, which (column) consists of DenseVectors.
+        k (int): The number of principal components to return.
+
+    Returns:
+        tuple of (np.ndarray, RDD of np.ndarray, np.ndarray): A tuple of (eigenvectors, `RDD` of
+        scores, eigenvalues).  Eigenvectors is a multi-dimensional array where the number of
+        rows equals the length of the arrays in the input `RDD` and the number of columns equals
+        `k`.  The `RDD` of scores has the same number of rows as `data` and consists of arrays
+        of length `k`.  Eigenvalues is an array of length d (the number of features).
+     """
+    cov = estimateCovariance(df)
+    col = cov.shape[1]
+    eigVals, eigVecs = eigh(cov)
+    inds = np.argsort(eigVals)
+    eigVecs = eigVecs.T[inds[-1:-(col+1):-1]]
+    components = eigVecs[0:k]
+    eigVals = eigVals[inds[-1:-(col+1):-1]]  # sort eigenvals
+    score = df.select(df['features']).map(lambda x: x[0]).map(lambda x: np.dot(x, components.T) )
+    # Return the `k` principal components, `k` scores, and all eigenvalues
+    return components.T, score, eigVals
+
+ def varianceExplained(df, k=1):
+     """Calculate the fraction of variance explained by the top `k` eigenvectors.
+
+     Args:
+         df: A Spark dataframe with a 'features' column, which (column) consists of DenseVectors.
+         k: The number of principal components to consider.
+
+     Returns:
+         float: A number between 0 and 1 representing the percentage of variance explained
+             by the top `k` eigenvectors.
+     """
+     components, scores, eigenvalues = pca(df, k)
+     return sum(eigenvalues[0:k])/sum(eigenvalues)
+
+    ### TEST
+
+from pyspark.ml.feature import *
+from pyspark.mllib.linalg import Vectors
+data = [(Vectors.dense([0.0, 1.0, 0.0, 7.0, 0.0]),),
+         (Vectors.dense([2.0, 0.0, 3.0, 4.0, 5.0]),),
+         (Vectors.dense([4.0, 0.0, 0.0, 6.0, 7.0]),)]
+df = sqlContext.createDataFrame(data,["features"])
+pca_extracted = PCA(k=2, inputCol="features", outputCol="pca_features")
+model = pca_extracted.fit(df)
+model.transform(df).collect()
+
+
+### Test with new model
+comp, score, eigVals = pca(df)
+score.collect()
