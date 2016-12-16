cf9ea845ca35891ac34822f0ff831737bcea8ebb
Nathan Smith <nathansforyou@gmail.com>
2016-12-16 07:57:36 -0500

    Trying ReLU activation, more training experiments

--- neural_network/protein_autoencoder.py
+++ neural_network/protein_autoencoder.py
@@ -57,10 +57,25 @@ display_step = 1
 
 # Network Parameters
 n_hidden_1 = 892 # 1st layer num features
-n_hidden_2 = 446 # 2nd layer num features
+n_hidden_2 = 227 # 2nd layer num features
 n_input = 2676 # protein data input (img shape: 892*3)
 
 X = tf.placeholder("float", [None, n_input])
+
+# testing with different weight initializations
+weights = {
+    'encoder_h1': tf.Variable(tf.random_normal([n_input, n_hidden_1], mean=0.0, stddev=0.5)),
+    'encoder_h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2], mean=0.0, stddev=0.5)),
+    'decoder_h1': tf.Variable(tf.random_normal([n_hidden_2, n_hidden_1], mean=0.0, stddev=0.5)),
+    'decoder_h2': tf.Variable(tf.random_normal([n_hidden_1, n_input], mean=0.0, stddev=0.5)),
+}
+biases = {
+    'encoder_b1': tf.Variable(tf.random_normal([n_hidden_1], mean=0.0, stddev=0.5)),
+    'encoder_b2': tf.Variable(tf.random_normal([n_hidden_2], mean=0.0, stddev=0.5)),
+    'decoder_b1': tf.Variable(tf.random_normal([n_hidden_1], mean=0.0, stddev=0.5)),
+    'decoder_b2': tf.Variable(tf.random_normal([n_input], mean=0.0, stddev=0.5)),
+}
+
 
 weights = {
     'encoder_h1': tf.Variable(tf.random_normal([n_input, n_hidden_1])),
@@ -75,7 +90,8 @@ biases = {
     'decoder_b2': tf.Variable(tf.random_normal([n_input])),
 }
 
-# Building the encoder
+
+# Building the encoder using a sigmoid activation function
 def encoder(x):
     # Encoder Hidden layer with sigmoid activation #1
     layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['encoder_h1']),
@@ -94,6 +110,28 @@ def decoder(x):
     layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, weights['decoder_h2']),
                                    biases['decoder_b2']))
     return layer_2
+
+
+# Another version using rectified linear units for activation
+def encoder(x):
+    # Encoder Hidden layer with relu activation #1
+    layer_1 = tf.nn.relu(tf.add(tf.matmul(x, weights['encoder_h1']),
+                                   biases['encoder_b1']))
+    # Decoder Hidden layer with relu activation #2
+    layer_2 = tf.nn.relu(tf.add(tf.matmul(layer_1, weights['encoder_h2']),
+                                   biases['encoder_b2']))
+    return layer_2
+
+# Building the decoder
+def decoder(x):
+    # Encoder Hidden layer with relu activation #1
+    layer_1 = tf.nn.relu(tf.add(tf.matmul(x, weights['decoder_h1']),
+                                   biases['decoder_b1']))
+    # Decoder Hidden layer with relu activation #2
+    layer_2 = tf.nn.relu(tf.add(tf.matmul(layer_1, weights['decoder_h2']),
+                                   biases['decoder_b2']))
+    return layer_2
+
 
 # Construct model
 encoder_op = encoder(X)
